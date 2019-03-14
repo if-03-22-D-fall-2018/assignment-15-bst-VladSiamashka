b09f454596d4942ab786fd0d3ecdffaef7bda02a
Siamashka Vlad <wladsemashko@gmail.com>
2019-03-14 09:46:05 +0100

    :)

--- bst.cpp
+++ bst.cpp
@@ -35,10 +35,12 @@ void delete_bst(Bst bst){
   }
 }
 
+
 int get_depth(Bst bst){
   if (bst==0)return 0;
-  if (bst->right==0&&bst->right==0)return 1;
-  return 2;
+  if (bst->right==0&&bst->left==0)return 1;
+  if (get_depth(left_subtree(bst))>get_depth(right_subtree(bst)))return 1+get_depth(left_subtree(bst));
+  return 1+get_depth(right_subtree(bst));
 }
 
 void add(Bst* bst, int value){
@@ -85,15 +87,53 @@ Bst right_subtree(Bst root){
 }
 
 int traverse_pre_order(Bst bst, int *elements, int start){
-  return 0;
+  if(bst != 0) {
+    elements[start] = bst->value;
+    start+=1;
+    start = traverse_pre_order(bst->left, elements, start);
+    start = traverse_pre_order(bst->right, elements, start);
+   }
+   return start;
 }
 
+/**
+*** Traverses the BST depth first by first traversing the left subtree, then adding the root,
+*** then traversing the right subtree.
+*** @param bst The BST to be traversed
+*** @param elements Array of elements in the order how they are found during traversal
+*** @param start Start index of elements wherer the function should start to add the found elements
+*** @return Number of elements found during traversal
+*/
 int traverse_in_order(Bst bst, int *elements, int start){
-  return 0;
+  if (bst!=0) {
+    start=traverse_in_order(bst->left, elements, start);
+    elements[start]=bst->value;
+    start+=1;
+    start=traverse_in_order(bst->right, elements, start);
+  }
+  return start;
 }
 
+/**
+*** Traverses the BST depth first by first traversing the left subtree, then traversing the right
+*** subtree and finally adding the root.
+*** @param bst The BST to be traversed
+*** @param elements Array of elements in the order how they are found during traversal
+*** @param start Start index of elements wherer the function should start to add the found elements
+*** @return Number of elements found during traversal
+*/
 int traverse_post_order(Bst bst, int *elements, int start){
-  return 0;
+  if (bst!=0) {
+    if(bst->left != 0){
+      start = traverse_post_order(bst->left, elements, start);
+    }
+    if(bst->right != 0){
+      start = traverse_post_order(bst->right, elements, start);
+    }
+    elements[start]=bst->value;
+    start+=1;
+  }
+  return start;
 }
 
 bool are_equal(Bst bst1, Bst bst2){
